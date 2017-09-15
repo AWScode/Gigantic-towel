dcf45ca12b7ffa5a5e9d105a75208dc8237e187f
Yichen Liu <yichen_liu@m018110.aw.org>
2017-09-15 13:26:02 -0700

    Kitty's 2nd upload

--- CoffeeShop/CoffeeShop.java
+++ CoffeeShop/CoffeeShop.java
@@ -1,20 +1,25 @@
+//Kitty Liu 2017.9.15
 import java.awt. *;
 import javax.swing.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.*;
-public class coffeeshop
+public class CoffeeShop
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to Kitty's Coffeeshop, may I take your order?");
+		System.out.println("Welcome to Kittenish Coffee, may I take your order?");
 		Scanner input= new Scanner(System.in);
 		String order=input.nextLine();
+
 		System.out.println("Can I have a name for that?");
-	    Scanner input2= new Scanner(System.in);
+	  Scanner input2= new Scanner(System.in);
 		String name= input2.nextLine();
+
 		System.out.println("One moment please....");
-        System.out.printf("%s! Your %s is ready", name, order);
+    System.out.printf("%s! Your %s is ready! ", name, order);
+
+		System.out.println("Have a nice day~");
 		System.exit(0);
 
 	}
