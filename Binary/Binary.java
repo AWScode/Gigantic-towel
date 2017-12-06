3538053d54a02fcd2778f015f9339c9b1b3fa0d1
Yichen Liu <yichen_liu@m018110.aw.org>
2017-12-06 13:30:30 -0800

    first push

---
+++ Binary/Binary.java
@@ -0,0 +1,23 @@
+//Kitty Liu
+//Due day: December 13th, 2017
+import java.util.*;
+
+public class Binary{
+
+
+
+  public static void main(String[] args) {
+    System.out.println("---Binary Calculator---");
+
+    //inputs
+    System.out.println("Input first binary number: ");
+    Scanner number1 = new Scanner(System.in);
+    System.out.println("Input second binary number: ");
+    Scanner number2 = new Scanner(System.in);
+    System.out.println("Choose (add, subtract, multiply, divide): ");
+    Scanner product = new Scanner(System.in);
+
+
+
+  }
+}
