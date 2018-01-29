d1b259fadc4dfda7a258e00f325a084effb22041
Yichen Liu <yichen_liu@m018110.aw.org>
2018-01-29 14:18:35 -0800

    first push

---
+++ Data.java
@@ -0,0 +1,23 @@
+import java.util.*;
+//class notes 20180129
+//data type
+
+
+public class Data {
+  public static void main(String[] args) {
+
+    //print 5 (integer) as a string data type
+    System.out.println("String: " + Integer.toString(5));
+
+
+
+  }
+
+
+
+
+
+
+
+
+}
