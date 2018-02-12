616ea6eba316725653ff3e2866ed0cb380025ad3
Yichen Liu <yichen_liu@m018110.aw.org>
2018-02-12 14:28:20 -0800

    first push, didn't finish

---
+++ Student.java
@@ -0,0 +1,49 @@
+//Kitty Liu
+//Due date: Friday, 2018.2.16
+
+public class Student {
+
+//all the info of student
+  public static String studentName;
+  public static String studentTieColor;
+  public static String studentBirthday;
+  public static String studentEthnicity;
+  public static int studentHeight;
+  public static int studentWeight;
+
+
+  public Student(String a, int a, String o) {
+    this.studentName = n;
+    this.studentAge = a;
+    this.studentTieColor = o;
+  }
+
+  public static void bark() {
+    System.out.println("Woof!");
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getOwner() {
+    return owner;
+  }
+
+  public static void setName(String n) {
+    name = n;
+  }
+
+  public static void setAge(int a) {
+    age = a;
+  }
+
+  public static void setOwner(String o) {
+    owner = o;
+  }
+
+}
