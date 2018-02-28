9d2a8629f5a68c982170558327cd001b9a87acc4
Yichen Liu <yichen_liu@m018110.aw.org>
2018-02-28 13:53:16 -0800

    first

---
+++ ConnectFour.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class ConnectFour{
+  //instance variables here
+  Array[] columes;
+
+  //7 rows
+  String[] colume1;
+  String[] colume2;
+  String[] colume3;
+  String[] colume4;
+  String[] colume5;
+  String[] colume6;
+  String[] colume7;
+  //bollean
+  Boolean gameOver;
+
+
+
+  public ConnectFour(){
+  //constructor method here
+  this.colume1 = new {"empty", "empty", "empty", "empty", "empty", "empty", "empty"};
+  this.colume2 = new {"empty", "empty", "empty", "empty", "empty", "empty", "empty"};
+  this.colume3 = new {"empty", "empty", "empty", "empty", "empty", "empty", "empty"};
+  this.colume4 = new {"empty", "empty", "empty", "empty", "empty", "empty", "empty"};
+  this.colume5 = new {"empty", "empty", "empty", "empty", "empty", "empty", "empty"};
+  this.colume6 = new {"empty", "empty", "empty", "empty", "empty", "empty", "empty"};
+  this.colume7 = new {"empty", "empty", "empty", "empty", "empty", "empty", "empty"};
+
+  this.columes = new Array{colume1, colume2, colume3, colume4, colume5, colume6, colume7};
+
+  this.gameOver = false;
+
+
+
+  }
+
+  public getPiece(){
+
+  }
+
+
+
+
+
+
+
+
+
+
+}
