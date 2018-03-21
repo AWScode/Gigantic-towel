59a5ec16153f036e80d9151c8e8546594614f3bc
Yichen Liu <yichen_liu@m018110.aw.org>
2018-03-21 12:14:12 -0700

    okie

--- Library.java
+++ Library.java
@@ -5,15 +5,16 @@ all the words in a book and can sort them in any
 way the user would like. This class can also
 return words that fit the search criteria. */
 import java.util.*;
+import java.io.*;
 
 public class Library {
 
   public ArrayList<String> words;//original list
-  public ArrayList<String> orgWords;//organized list
+  public ArrayList<String> sortList;//organized list
 
   public Library() {
     this.words = new ArrayList<String>();
-    this.orgWords = new ArrayList<String>();
+    this.sortList = new ArrayList<String>();
 
     //Sample
     words.add("once");
@@ -41,12 +42,84 @@ public class Library {
 
   }
 
+  public int findLength(String a){
+    int length = a.length();
+    return length;
+  }
+
+
+
+  public void printList(ArrayList<String> printL){
+    for (int i = 0; i <= printL.size(); i++) {
+      String tempForPrint = printL.get(i);
+      System.out.println(tempForPrint);
+    }
+  }
+
+  public ArrayList<String> mergeSort(ArrayList<String> words){
+    ArrayList<String> a = new ArrayList<String>();
+    ArrayList<String> b = new ArrayList<String>();
+    int high = words.size();//number of item in the list
+    int low = 0;
+    int mid = (high - low)/2;
+    //int trackWhenToStop = high;//default
+
+    if (high == 1) {//if there is only one term
+      return words;
+    }else if (high < 1){
+      System.out.println("The list doesn't exist");
+    }else{
+      //splitting part
+      while(true){
+        //split
+        high = high/2;
+        //add items into new ArrayList
+        for (int i = low; i <= mid; i++) {
+          a.set(i, words.get(i));
+        }
+        for (int j = mid + 1; j <= high; j++) {
+          b.set(j, words.get(j));
+        }
+
+        if (high == 1) {
+          break;
+        }
+      }
+
+      //merging part
+      ArrayList<String> c = new ArrayList<String>();
+      int ii = 0;
+      int jj = 0;
+
+      while(true){
+        if(findLength(a.get(ii)) < findLength(b.get(jj))){
+          c.add(a.get(ii));
+          ii++;
+        }else{
+          jj++;
+        }
+        if (jj == b.size()) {
+          c.add(b.get(jj));
+        }
+      }
+      //it reported error after successfully compiled
+      //really don't know if it works...........
+      //seems like it doesn't
+
+
+
+    }
+
+
+    return c;
+  }
+
+
   //???????needed??????comparing strings by their alphabetical order
   public static int unicode(String a){
     //converting a String to int in unicode
     char[] uniCharacter = a.toCharArray();
     int uniInt = Integer.parseInt(new String(uniCharacter));
-
 
     return uniInt;
   }
@@ -81,39 +154,32 @@ public class Library {
 
   }
   //Sort with Selection
-  public void wLengthSort(String[] words, String[] orgWords){
-
-    // for (int i = 0; i < words.length-1; i++){
-    //   for (int j = i+1; j < words.length; j++)
-    //             if (words[j] < words[index])
-    //                 index = j;
-    //
-    //         int smallerNumber = words[index];
-    //         words[index] = words[i];
-    //         words[i] = smallerNumber;
-    // }
+  public ArrayList<String> wLengthSort(ArrayList<String> words, ArrayList<String> sortList){
 
 
-    String track  = words[0]; //track the smallest one
-    int c = 0;//count
-    //find the minimum
-    for (int i = 0; i < words.length; i++){
-      if (unicode(track) < unicode(words[i])) {
-        //stay
-        orgWords[c] = track;
-        c = c + 1;
-      }else if (unicode(track) > unicode(words[i])) {
-
-        track = words[i];
-        orgWords[c] = track;
-        c = c + 1;
-        //remove words[i] from the words[]
-
-
-
-      }
+    ArrayList<String> tempList = new ArrayList<String>();
+    for (int h = 0; h < words.size(); h++ ) {//duplicating the list
+      //purpose: to keep the length of the original list
+      tempList.set(h, words.get(h));
     }
+
+    for (int i = 0; i < words.size(); i++ ) {//number of minumum we need
+      String min = tempList.get(0);//track the minumum, always start from the 1st item
+      for (int j = 0; j < tempList.size() ; j++ ) {//the method to find the minimum
+        if (min.length() > tempList.get(j).length()) {
+          min = tempList.get(j);
+        }
+      }
+      tempList.remove(min);
+      sortList.add(min);
+    }
+    return sortList;
   }
+
+
+
+
+
 
   public int getWordCount(String w){
     int count = 0;
@@ -129,6 +195,16 @@ public class Library {
     ArrayList<String> topWords = new ArrayList<String>();
 
     return topWords;
+  }
+
+  public void readNewBook(String filename){
+    File newBook = new File(filename);
+    try{
+      FileReader fr = new FileReader(newBook);
+    }
+    catch (FileNotFoundException e){
+      System.out.println("The file is not found.");
+    }
   }
 
 
@@ -147,6 +223,10 @@ public class Library {
     System.out.println("Word: there : " + Integer.toString(myLib.getWordCount("there")));
     System.out.println("Word: castle : " + Integer.toString(myLib.getWordCount("castle")));
     System.out.println("Word: once : " + Integer.toString(myLib.getWordCount("once")));
+
+    myLib.readNewBook("Emma");
+
+
 
   }
 
